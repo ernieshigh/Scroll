cbb539d7b6a78b22fe98777d1c232eb5ffd1ed71
ernieshigh <ernie@ernieshigh.dev>
2021-09-28 04:04:32 +0800

    fix add_post_meta

--- Import Post Meta/import-postmeta.php
+++ Import Post Meta/import-postmeta.php
@@ -16,26 +16,26 @@
 	   if(isset($meta_field[1])){
 			update_post_meta($post_id, 'dummy_type', $meta_field[1]);
 		}else{
-			add_user_meta($post_id, 'dummy_type', $meta_field[1]);
+			add_post_meta($post_id, 'dummy_type', $meta_field[1]);
 		}
 		if(isset($meta_field[2])){
 			update_post_meta($post_id, 'dum_symbol', $meta_field[2]);
 		}else{
-			add_user_meta($post_id, 'dum_symbol', $meta_field[2]);
+			add_post_meta($post_id, 'dum_symbol', $meta_field[2]);
 		}
 		if(isset($meta_field[3])){
 			update_post_meta($post_id, 'dum_start_date', $meta_field[3]);
 		}else{
-			add_user_meta($post_id, 'dum_start_date', $meta_field[3]);
+			add_post_meta($post_id, 'dum_start_date', $meta_field[3]);
 		}
 	   if(isset($meta_field[4])){
 			update_post_meta($post_id, 'dum_stop_date', $meta_field[4]);
 		}else{
-			add_user_meta($post_id, 'dum_stop_date', $meta_field[4]);
+			add_post_meta($post_id, 'dum_stop_date', $meta_field[4]);
 		}
 		if(isset($meta_field[5])){
 			update_post_meta($post_id, 'dum_message', $meta_field[5]);
 		}else{
-			add_user_meta($post_id, 'dum_message', $meta_field[5]);
+			add_post_meta($post_id, 'dum_message', $meta_field[5]);
 		}
 	}		
